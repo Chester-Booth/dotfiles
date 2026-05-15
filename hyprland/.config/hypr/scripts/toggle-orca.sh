2d50c602fa611207ff18268231f1cb1835b9dfdd
Chester <105993308+Chester-Booth@users.noreply.github.com>
2026-05-15 17:20:26 +0100

    toggle orca addded

---
+++ hyprland/.config/hypr/scripts/toggle-orca.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -eu
+
+if pgrep -x orca >/dev/null 2>&1; then
+    pkill -x orca
+    notify-send -u low -i preferences-desktop-accessibility "Orca disabled"
+else
+    orca --replace >/dev/null 2>&1 &
+    notify-send -u low -i preferences-desktop-accessibility "Orca enabled"
+fi
